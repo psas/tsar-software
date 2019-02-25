c613df04ea29208420ebd8fb4c72c9709f34fe1a
Ryan Medick <rmedick@pdx.edu>
2019-02-25 13:16:42 -0800

    Added more comments to fixed_queue

--- CentralManager/src/fixed_queue.hpp
+++ CentralManager/src/fixed_queue.hpp
@@ -4,26 +4,43 @@
 #include <mutex>
 #include <vector>
 
-/*
- * This class implament ad fixed length queue by a using an array to 
+
+/* Why this class is needed:
+ * The Link Logger and Server both need queues to act as buffer for data to be sent and
+ * data recieved. Since it is possible for the Sequencer and Hardware Controller to send
+ * out their status/data faster than the Link Logger (and Server) can process that data,
+ * a fixed queue is needed for the send queues. A std::queue would could endless grow 
+ * in a worse case scenario.
+ *
+ * How this Class works:
+ * This class implements a fixed length queue by a using an vector as a circular array.
+ * The vector length is set during the constructor and can never change after. 
+ * If the queue is full, the oldest enqueue data will be overriden when a new enqueue 
+ * occurs. 
  */
 template <typename T>
 class fixed_queue {
     public:
+        // default constructor
         fixed_queue(const unsigned int in_q_len);
+
+        // fill constructor
         fixed_queue(const unsigned int in_q_len, const T & input);
 
         int enqueue(const T & input);
         int dequeue(T & output);
-        int status();
+
+        // return number of items in queue currently
+        int size();
     private:
         std::vector<T> data;
-        unsigned int length;
-        unsigned int current_count;
-        unsigned int current_enqueue;
-        unsigned int current_dequeue;
+        unsigned int length;                // size of container (the vector length)
+        unsigned int current_count;         // count of data enqueued
+        unsigned int current_enqueue;       // current enqueue posistion in vector
+        unsigned int current_dequeue;       // current dequeue posistion in vector
         std::mutex queue_mutex;
 };
+
 
 // constructor, with vector default constructor
 template <typename T>
@@ -32,13 +49,13 @@ fixed_queue(const unsigned int in_q_len) :
         data(in_q_len+1), length(in_q_len), current_count(0), 
         current_enqueue(0), current_dequeue(0) {}
 
+
 // constructor, with vector fill constructor
 template <typename T>
 fixed_queue<T>::
 fixed_queue(const unsigned int in_q_len, const T & input) : 
         data(in_q_len+1, input), length(in_q_len), current_count(0), 
         current_enqueue(0), current_dequeue(0) {}
-
 
 
 // enqueue data, will not fail
@@ -61,6 +78,7 @@ enqueue(const T & input) {
     queue_mutex.unlock();
     return 1;
 }
+
 
 // dequeue data
 // if empty return 0, otherwise return 1
@@ -86,7 +104,7 @@ dequeue(T & output) {
 // get current count of items in the queue
 template <typename T>
 int fixed_queue<T>::
-status() {
+size() {
     int rc = -1;
     queue_mutex.lock();
     rc = current_count;
