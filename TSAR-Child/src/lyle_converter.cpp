4ead801c45273a0ba7ec6388938da88c3ab9e380
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2019-08-02 20:22:19 -0700

    updated btop and btot functions

--- TSAR-Child/src/lyle_converter.cpp
+++ TSAR-Child/src/lyle_converter.cpp
@@ -5,21 +5,27 @@
 //
 LyleConverter::LyleConverter() : board("/dev/i2c-1", 0x68, 0x69, 0x6a, 0x6b){}
 
+float LyleConverter::binary_to_pressure(const int data){ return data / 2; }
+
+float LyleConverter::binary_to_temperature(const int data){ return data / 2; }
+
 LyleConverter::load_state(const State& state){
   int* data;
   board.read_adcs(data);
 
   state.state_mutex.lock(); // Lock the mutex
 
-  state.pressure_1 = binary_to_pressure(data[0]);
-  state.pressure_2 = binary_to_pressure(data[4]);
-  state.pressure_3 = binary_to_pressure(data[8]);
-  state.pressure_4 = binary_to_pressure(data[12]);
+  if(data){
+    state.pressure_1 = binary_to_pressure(data[0]);
+    state.pressure_2 = binary_to_pressure(data[4]);
+    state.pressure_3 = binary_to_pressure(data[8]);
+    state.pressure_4 = binary_to_pressure(data[12]);
 
-  state.temp_1 = binary_to_temperature(data[1]);
-  state.temp_2 = binary_to_temperature(data[5]);
-  state.temp_3 = binary_to_temperature(data[9]);
-  state.temp_4 = binary_to_temperature(data[13]);
+    state.temp_1 = binary_to_temperature(data[1]);
+    state.temp_2 = binary_to_temperature(data[5]);
+    state.temp_3 = binary_to_temperature(data[9]);
+    state.temp_4 = binary_to_temperature(data[13]);
+  }
 
   state.state_mutex.unlock(); // Release the mutex
 
--- TSAR-Child/src/lyle_converter.h
+++ TSAR-Child/src/lyle_converter.h
@@ -4,6 +4,9 @@
 #include "state.h"
 #include "tsar_lowrate_daq.h"
 
+//
+// This is responsible for reading the ADC motherboard and all the ADC chips and loads the State structure declared inside state.h
+//
 class LyleConverter {
   private:
     lowrate_daq board;
