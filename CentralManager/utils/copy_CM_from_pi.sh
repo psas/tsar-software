b145d20c44cc4bf4ed22a90bae69af67b3b25e0a
ryanpdx <rmedick@pdx.edu>
2019-10-20 18:16:04 -0700

    merge tsar-child with central manager, made rapidjson and BBB gpio into submodules

--- CentralManager/utils/copy_CM_from_pi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#only used to copy cpp/h file from pi to host computer
-
-rsync -cvazP pi@192.168.0.14:~/CentralManager/* ../
--- CentralManager/utils/send_CM_to_pi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#only used to copy cpp/h file to pi from host computer
-
-rsync -cvazP ../../CentralManager pi@192.168.0.14:~/.
