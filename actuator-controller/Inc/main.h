0171399d086f6c41402942df3de3bb88e652a3ef
austinjoseph <austinjosephbw@hotmail.com>
2020-12-02 20:30:40 -0800

    Including Pin defs

--- actuator-controller/Inc/main.h
+++ actuator-controller/Inc/main.h
@@ -59,7 +59,32 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-
+#define LOW_POWER_UART2_RX_Pin GPIO_PIN_0
+#define LOW_POWER_UART2_RX_GPIO_Port GPIOC
+#define LOW_POWER_UART1_TX_Pin GPIO_PIN_1
+#define LOW_POWER_UART1_TX_GPIO_Port GPIOC
+#define GPIO13_Pin GPIO_PIN_13
+#define GPIO13_GPIO_Port GPIOB
+#define GPIO14_Pin GPIO_PIN_14
+#define GPIO14_GPIO_Port GPIOB
+#define GPIO15_Pin GPIO_PIN_15
+#define GPIO15_GPIO_Port GPIOB
+#define GPIO8_Pin GPIO_PIN_8
+#define GPIO8_GPIO_Port GPIOA
+#define GPIO9_FAST_Pin GPIO_PIN_9
+#define GPIO9_FAST_GPIO_Port GPIOA
+#define GPIO6_Pin GPIO_PIN_6
+#define GPIO6_GPIO_Port GPIOC
+#define GPIO7_Pin GPIO_PIN_7
+#define GPIO7_GPIO_Port GPIOC
+#define GPIO8D8_Pin GPIO_PIN_8
+#define GPIO8D8_GPIO_Port GPIOD
+#define GPIO9_Pin GPIO_PIN_9
+#define GPIO9_GPIO_Port GPIOD
+#define GPIO10_FAST_Pin GPIO_PIN_10
+#define GPIO10_FAST_GPIO_Port GPIOA
+#define GPIO2_Pin GPIO_PIN_2
+#define GPIO2_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
