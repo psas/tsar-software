50f02376a92b32db43b4f3a631b52fcd2db1f075
Katie <56359756+kloseke@users.noreply.github.com>
2021-03-26 13:25:34 -0700

    Add files via upload

---
+++ valve_indicator/Current_VI_Code/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32l476rgtx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32l476rgtx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32l476rgtx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/startup_stm32l476rgtx.o: ../Core/Startup/startup_stm32l476rgtx.s
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32l476rgtx.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
